203b4db8515708f219215f41464aef11ce50e3e0
drySeaWind <sami.volotinen@gmail.com>
2018-06-28 10:07:34 +0300

    wildfly swarm version updated

---
+++ src/main/java/fi/sami/service/common/RestApplication.java
@@ -0,0 +1,15 @@
+/*
+ * 2018
+ */
+package fi.sami.service.common;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * @author Sami
+ */
+@ApplicationPath("/wordcounter/rest")
+public class RestApplication extends Application {
+}
